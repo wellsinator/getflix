4db3033fd2fc7e179485c76cda2d00d40f5877c5
AlexWellsHS <alex@headspace.com>
2018-02-03 15:37:02 -0800

    get webpack, babel, react, express hooked up

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: ['transform-class-properties']
+        }
+      },
+      {
+        test: /\.css/,
+        include: SRC_DIR,
+        loaders: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
